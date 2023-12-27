6c0b91571c2e81476401f4d741a4acc7c677a3ed
DBulacio <daniel.bulacio@hotmail.com>
2023-12-27 00:12:06 -0300

    adding home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
 
 const HomePage = () => {
   return (
     <div>
-      HomePage
+      <Image className='home-bg' src="home_bg.jpg" alt="main background for homepage" fluid />
+      <div className="black-overlay"></div>
+      <span className='home-text'>
+        <p className='home-pg-1'>CARPET & FLOOR CLEANING</p>
+        <h1 className='home-title'>STAR CLEAN UP</h1>
+        <p className='home-pg-2'>PREMIUM FLOOR CARE SERVICES IN MIAMI-DADE - BROWARD AND PALM BEACH</p>
+        <Button className='main-button'>CALL US TODAY XXX-XXX-XXXX</Button>
+      </span>
     </div>
   )
 }
