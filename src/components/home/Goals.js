b7a55e7c68593c3d4d9d262db4580345e0f61592
DBulacio <daniel.bulacio@hotmail.com>
2023-12-28 17:21:31 -0300

    adding Goals component

--- src/components/home/Goals.js
+++ src/components/home/Goals.js
@@ -2,8 +2,53 @@ import React from "react";
 
 const Goals = () => {
   return (
-    <>
-    </>
+    <div className="row">
+      {/* <div className="services-content col-md-6">
+        <h2 className="services-title">OUR FLOOR CARE SERVICES</h2>
+        <p className="services-pg"><span className="starcleanup">Star Clean Up</span> offers specialized range of services directed to high end residential and commercial facilities in need of a long term solution to keep their floors and textiles looking the best and expanding lifespan as much as possible by ensuring only the best procedures, equipment and diamond embedded pads are used in all and every restoration or maintenance job</p>
+        <ul className="services-list">
+          <li className="services-list-item">
+            <a href="#"> Terrazzo Cleaning & Polishing</a>
+          </li>
+          <li className="services-list-item">
+            <a href="#"> Grout Repairs & Re-grouting</a>
+          </li>
+          <li className="services-list-item">
+            <a href="#"> Travertine Cleaning, Polishing and Sealing</a>
+          </li>
+          <li className="services-list-item">
+            <a href="#"> Limestone Cleaning, Polishing and Sealing</a>
+          </li>
+          <li className="services-list-item">
+            <a href="#"> Shower Restoration Services</a>
+          </li>
+          <li className="services-list-item">
+            <a href="#"> Nano Ceramic Coatings</a>
+          </li>
+          <li className="services-list-item">
+            <a href="#"> Commercial Carpet Cleaning</a>
+          </li>
+          <li className="services-list-item">
+            <a href="#"> Commercial Upholstery Cleaning</a>
+          </li>
+        </ul>
+        <Button className='main-button get-quote-button'>GET QUOTE</Button>
+      </div> */}
+
+      {/* <div className="services-image col-md-6">
+        <Carousel>
+          <Carousel.Item>
+            <img src="carousel_1.jpeg"/>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src="carousel_2.jpeg"/>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src="carousel_3.jpeg"/>
+          </Carousel.Item>
+        </Carousel>
+      </div> */}
+    </div>
   )
 }
 
