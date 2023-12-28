67027b561fb311029ca549f1b756cb5bb2d143db
DBulacio <daniel.bulacio@hotmail.com>
2023-12-28 17:18:16 -0300

    adding services styling

--- src/components/home/Services.js
+++ src/components/home/Services.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
 
 const Services = () => {
   return (
     <div className="home-services-bg row">
-      <div className="services-content col">
+      <div className="services-content col-md-6">
         <h2 className="services-title">OUR FLOOR CARE SERVICES</h2>
         <p className="services-pg"><span className="starcleanup">Star Clean Up</span> offers specialized range of services directed to high end residential and commercial facilities in need of a long term solution to keep their floors and textiles looking the best and expanding lifespan as much as possible by ensuring only the best procedures, equipment and diamond embedded pads are used in all and every restoration or maintenance job</p>
         <ul className="services-list">
@@ -33,10 +34,12 @@ const Services = () => {
             <a href="#"> Commercial Upholstery Cleaning</a>
           </li>
         </ul>
+        <Button className='main-button get-quote-button'>GET QUOTE</Button>
       </div>
 
-      <div className="services-image col">
-        {/* <Carousel className="services-image-carousel">
+      <div className="services-image col-md-6">
+        <Carousel>
+          {/* It would be nice to have the iamges be square to fit the text */}
           <Carousel.Item>
             <img src="carousel_1.jpeg"/>
           </Carousel.Item>
@@ -46,7 +49,7 @@ const Services = () => {
           <Carousel.Item>
             <img src="carousel_3.jpeg"/>
           </Carousel.Item>
-        </Carousel> */}
+        </Carousel>
       </div>
     </div>
   )
